e2f8c732066f77d052abfba1e005945442900a24
Agii Uri <Agii Uri>
2017-02-03 10:01:34 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+ function getTriangleArea(a, h) {
+	 if ((a <=0 ) || (h <=0)) {
+		 return 'Nieprawidłowe dane'
+	}
+	 else {
+	 return a*h/2
+	}	
+}
+
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(8, 4);
+var triangle2Area = getTriangleArea(12, 7);
+var triangle3Area = getTriangleArea(9, 10);
+
+console.log(getTriangleArea(8, 4));
+console.log(getTriangleArea(12, 7));
+console.log(getTriangleArea(9, 10));
